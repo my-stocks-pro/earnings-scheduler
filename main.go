43f4a1e6aba222d6e0c5fbc3ff254c9832e954ce
Olexandr Rizhiy <orizhiy@e1r11p2.unit.ua>
2018-07-29 20:55:47 +0300

    service init

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/my-stocks-pro/earnings-scheduler/scheduler"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	router := gin.Default()
+
 	Scheduler := scheduler.New()
 
-	Scheduler.Run()
+	go Scheduler.Run()
+
+	router.Run(":8002")
 }
